40118b20f5b31465d5a4482fbbd5eef8b06bf895
jonghyuck.shin <jonghyuck.shin@lge.com>
2021-07-14 13:20:27 +0900

    monitor update
    - add 3s delay before child process is restarted
    - modify some logs

--- Server/src/Monitor.cpp
+++ Server/src/Monitor.cpp
@@ -10,33 +10,44 @@ int main(int argc, char *argv[])
     int status;
     int child_pid;
     pid_t pid;
+    int reboot_cnt = -1;
 
-    printf("Monitor Arguments: Argv[1](%s), Argv[2](%s)\n", argv[1], argv[2]);
+    printf("[Availability] =============================================\n");
+    printf("[Availability] Monitor Arguments: Argv[1](%s), Argv[2](%s)\n", argv[1], argv[2]);
+    printf("[Availability] =============================================\n");
 
     while(1) {
         pid = fork();
+        reboot_cnt++;
 
         if (pid < 0) {
             printf("[Error] fork failed!\n");
         }
         else if (pid == 0) { // child
-            //execl("LgFaceRecDemoTCP", "LgFaceRecDemoTCP", "8000", "friends_1280x720_12fps.smjpeg", NULL);
+            if (reboot_cnt > 0) {
+                printf("[Availability] =============================================\n");
+                printf("[Availability] The ImageProcessingApplication is restarted !\n");
+                printf("[Availability] =============================================\n");
+            }
             execl("LgFaceRecDemoTCP", "LgFaceRecDemoTCP", argv[1], argv[2], NULL);
             exit(1);
         }
         else { // parent
-            printf("Parent process is going to wait!\n");
+            printf("[Availability] =============================================\n");
+            printf("[Availability] Parent process is going to wait!\n");
+            printf("[Availability] =============================================\n");
             child_pid = wait(&status);
-    	    printf("child exit value : %d\n", WEXITSTATUS(status));
+
+            printf("[Availability] =============================================\n");
+    	    printf("[Availability] Child exit value : %d\n", WEXITSTATUS(status));
+            printf("[Availability] =============================================\n");
             if (WEXITSTATUS(status) == 3) {
                 printf("Start to rescan\n");
                 system("./rescan.sh");
             }
-            else {
-                printf("-----------------------------------\n");
-                printf("[DEBUG] Child(LgFaceRecDemoTCP) is killed\n");
-                printf("[DEBUG] Child(LgFaceRecDemoTCP) pid = %d\n", child_pid);
-            }
+
+            // delay 3s before image processing application is restarted.
+            sleep(3);
         }
     }
 
